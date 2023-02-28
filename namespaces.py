1d5c86365ac3b4130b551f6f036508aa5affc9dc
not00100001 <126187133+not00100001@users.noreply.github.com>
2023-02-28 19:31:34 +0100

    Create namespaces.py

---
+++ namespaces.py
@@ -0,0 +1,8 @@
+my_var = 10
+
+def my_function():
+    my_var = 20
+    print(f"You're telling me my_var will show different values? {my_var} ")
+
+my_function()
+print(f"Holy moly, it works! {my_var}")
