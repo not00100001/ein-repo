2cb81a80d9bf90d38f01835a8cd927667aa521b2
not00100001 <126187133+not00100001@users.noreply.github.com>
2023-03-04 21:04:37 +0100

    Create P4E_exercise6.1teachersolution.py

---
+++ P4E_exercise6.1teachersolution.py
@@ -0,0 +1,23 @@
+# Exercise 5: Take the following Python code that stores a string:`
+
+# str = 'X-DSPAM-Confidence:0.8475'
+
+# Use find and string slicing to extract the portion of the string after the colon 
+# character and then use the float function to convert the extracted 
+# string into a floating point number.
+
+data_str = "X-DSPAM-Confidence:0.8475"
+
+start_float = data_str.find(":")
+
+piece = data_str[start_float+1:]
+
+print(float(piece))
+
+# end_float = data_str.find("5", start_float)
+
+# our_float = data_str[start_float+1 : end_float+1]
+
+# print(float(our_float))
+
+
