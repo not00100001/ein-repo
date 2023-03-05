5fc908f48c99fbda77c5643df11174d913896ed6
not00100001 <126187133+not00100001@users.noreply.github.com>
2023-03-05 19:42:34 +0100

    Create forloop2.py

---
+++ forloop2.py
@@ -0,0 +1,8 @@
+friends = ["Bobby", "Glenn", "Susan"]
+
+for i in range(len(friends)):
+    friend = friends[i]
+    print(f"Hello {friend}!")
+
+# this way of looping apparently is used when you need to keep track of your loop
+# or something. Just remember it.
