360ef9e31293f5e80f03814fad690dfe429a98e2
not00100001 <126187133+not00100001@users.noreply.github.com>
2023-03-06 23:17:43 +0100

    Create tuff_sorting.py

---
+++ tuff_sorting.py
@@ -0,0 +1,8 @@
+tuff_stuff = [3, 1, 4, 5, 7, 0, 20, 10, 14, 8, 9]
+
+tuff_stuff.sort()
+
+print(tuff_stuff)
+
+# I swear today was a tough day. This is all I could do. It's still a new 
+# thing I learned
